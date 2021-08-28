af61a39b91fa8f62d54a2e6dfc8b1e98cf16aff4
sandeepjhaVC <heyyoumailmeat@gmail.com>
2021-08-28 10:48:16 +0530

    update

--- sketch.js
+++ sketch.js
@@ -1,93 +1,188 @@
-var driver
-
+var driver, car;
 var bgimg, bg;
-
-var ride;
-
 var rides = 0;
-var completed = 0
+var completed = 0;
+
+var ride1img, ride2img, ride3img;
+var right, left;
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var engine;
+
+var rideGroup;
+
+var obs1, obs2, ob3, obs4;
 
 function preload() {
-  bgimg = loadImage("Road.png");
+  bgimg = loadImage("Images/Road.png");
+
+  car = loadImage("Images/car/car1.png");
+  right = loadImage("Images/car/carRight.png");
+  left = loadImage("Images/car/carLeft.png");
+
+  ride1img = loadImage("Images/Rides/ride1.png");
+  ride2img = loadImage("Images/Rides/ride2.png");
+  ride3img = loadImage("Images/Rides/ride3.png");
+
+  obs1 = loadImage("Images/DownObs/carDon1.png");
+  obs2 = loadImage("Images/DownObs/carDon2.png");
+  obs3 = loadImage("Images/UpObs/carUp1.png");
+  obs4 = loadImage("Images/UpObs/carUp2.png");
+  
+  engine = loadSound("Sounds/Engine.mp3");
 }
 
 function setup() {
-  createCanvas(displayWidth - 30, displayHeight);
+  createCanvas(2000, 1000);
 
-  
-
-  bg = createSprite(100, 100, 50, 10);
+  bg = createSprite(1000, 500);
   bg.addImage(bgimg);
-  bg.velocityY = 4;
+  bg.scale = 0.5;
+  bg.velocityY += 3;
 
-  driver = createSprite(200, 200, 50, 50);
+  driver = createSprite(1000, 800);
+  driver.addImage(car);
 
-  ride = createSprite(400,200,30,20);
+  rideGroup = new Group();
 
 }
 
 function draw() {
   background(0);
 
-  camera.position.x = driver.x;
-  camera.position.y = driver.y;
+  if (bg.y > 1000) {
+    bg.y = 500
+  }
 
-  
-
-  if (bg.y > displayHeight) {
-    bg.y = displayHeight / 2;
+  if (driver.y < -1060) {
+    bg.y = -1060;
   }
 
   if (keyWentDown('w')) {
     driver.velocityY = -5;
+
+
   }
 
   if (keyWentUp('w')) {
     driver.velocityY = 0;
+
   }
 
   if (keyWentDown('s')) {
     driver.velocity.y += 5;
+
   }
 
   if (keyWentUp('s')) {
     driver.velocity.y = 0;
+
   }
 
   if (keyWentDown('a')) {
     driver.velocity.x -= 5;
+    driver.addImage(left);
+
   }
 
   if (keyWentUp('a')) {
     driver.velocity.x = 0;
+    driver.addImage(car);
+
   }
 
   if (keyWentDown('d')) {
     driver.velocity.x += 5;
+    driver.addImage(right);
+
   }
 
   if (keyWentUp('d')) {
     driver.velocity.x = 0;
+    driver.addImage(car);
+
   }
 
-  
-
-  spawnRider();
+  spawnRide();
+  spawnCar();
 
   drawSprites();
 
   textSize(30);
   fill("red")
-  text("Total rides: "+ completed,10,10);
+  text("Total rides: " + completed, 100, 100);
 
 }
 
-function spawnRider() {
-  if(driver.isTouching(ride)){
-    ride.destroy();
-    ride = createSprite(200,random(100,500),20,30);
-    completed ++;
-    rides = 1;
+function spawnRide() {
+  if (frameCount % 500 === 0) {
+    var ride = createSprite(650, -100);
+    ride.velocityY += 3;
+
+    var rand = Math.round(random(1, 3));
+    switch (rand) {
+      case 1: ride.addImage(ride1img);
+        break;
+      case 2: ride.addImage(ride2img);
+        break;
+      case 3: ride.addImage(ride3img);
+      default: break;
+    }
+
+    ride.scale = 0.8;
+    //ride.lifetime = 300;
+    rideGroup.add(ride);
+  }
+  if (frameCount % 300 === 0) {
+    var ride2 = createSprite(1350, -100);
+
+    var rand = Math.round(random(1, 3));
+    switch (rand) {
+      case 1: ride2.addImage(ride1img);
+        break;
+      case 2: ride2.addImage(ride2img);
+        break;
+      case 3: ride2.addImage(ride3img);
+      default: break;
+    }
+
+    ride2.velocityY += 3;
+    ride2.scale = 0.7;
+    //ride2.lifetime = 50;
+    rideGroup.add(ride2);
   }
 
+}
+
+function spawnCar() {
+  if (frameCount % 700 === 0) {
+    var obs = createSprite(800, -100);
+
+    var rand = Math.round(random(1, 2));
+    switch (rand) {
+      case 1: obs.addImage(obs1);
+        break;
+      case 2: obs.addImage(obs2);
+        break;
+      default: break;
+    }
+
+    obs.velocityY += 3;
+  }
+  if (frameCount % 800 === 0) {
+    var obss = createSprite(1200, 1200);
+
+    var rand = Math.round(random(1, 2));
+    switch (rand) {
+      case 1: obss.addImage(obs3);
+        break;
+      case 2: obss.addImage(obs4);
+        break;
+      default: break;
+    }
+
+    obss.velocityY -= 3;
+  }
 }
